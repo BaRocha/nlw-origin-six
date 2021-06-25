f59e04d15a36f746ede26faf691f6a1202de83fe
barocha <barbara.rocha07@yahoo.com>
2021-06-25 17:31:37 -0300

    setup

---
+++ main.js
@@ -0,0 +1,19 @@
+/*Abre e fecha o menu*/
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+//console.log(toggle)
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+/*quando clicar em um item do menu, esconder o menu*/
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
