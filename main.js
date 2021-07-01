e22c8fb5d8c02972855bd3cdb196d0d8f44f6079
barocha <barbara.rocha07@yahoo.com.br>
2021-07-01 18:15:38 -0300

    create mobile version

--- main.js
+++ main.js
@@ -1,19 +1,60 @@
 /*Abre e fecha o menu*/
-const nav = document.querySelector('#header nav')
-const toggle = document.querySelectorAll('nav .toggle')
+const nav = document.querySelector("#header nav");
+const toggle = document.querySelectorAll("nav .toggle");
 //console.log(toggle)
 
 for (const element of toggle) {
-  element.addEventListener('click', function () {
-    nav.classList.toggle('show')
-  })
+  element.addEventListener("click", function () {
+    nav.classList.toggle("show");
+  });
 }
 
 /*quando clicar em um item do menu, esconder o menu*/
-const links = document.querySelectorAll('nav ul li a')
+const links = document.querySelectorAll("nav ul li a");
 
 for (const link of links) {
-  link.addEventListener('click', function () {
-    nav.classList.remove('show')
-  })
+  link.addEventListener("click", function () {
+    nav.classList.remove("show");
+  });
 }
+
+/* Mudar o header da pagina quando der scroll*/
+const header = document.querySelector("header");
+const navHeight = header.offsetHeight;
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= navHeight) {
+    //scroll é maior que a altura do header
+    header.classList.add("scroll");
+  } else {
+    //scro é menor que a altura do header
+    header.classList.remove("scroll");
+  }
+});
+
+/*Testimonials carousel slider swiper*/
+const swiper = new Swiper(".swiper-container", {
+  slidesPerView: 1,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  mousewheel: true,
+  keyboard: true,
+});
+
+/* ScrollReveal: Mostrar elementos quando der scroll na página */
+const scrollReveal = ScrollReveal({
+  origin: "top",
+  distance: "30px",
+  duration: 700,
+  reset: true,
+});
+
+scrollReveal.reveal(
+  `#home .text, #home .image,
+  #about .text, #about .image,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials,
+  #contact .text, #contact .links`,
+  { interval: 100 }
+);
